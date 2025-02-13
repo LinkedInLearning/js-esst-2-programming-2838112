75a32137631d4d5b66073094cc0623655d1d4225
Ralph Steyer <ralph.steyer@rjs.de>
2025-02-13 10:06:52 +0000

    ChallengeSolution02

---
+++ kap02/lib/js/challenge2.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// Ein Array liegt vor
+let numbers = [1, 2, 3, 4, 5];
+
+/*
+Die folgenden Schritte 1 - 3 sollen durch Methodenverkettung als eine Anweisung durchgeführt werden. Dabei kann jeweils die Lambda-Notation verwendet werden.
+*/
+// 1. Jedes Element im Array soll vom Wert verdoppelt werden 
+
+// 2. Danach filtern aller Zahlen > 5 und Rückgabe des Ergebnisses
+
+// 3. Reduzieren des Arrays auf einzelnen Wert mit der Summe der Elemente
+
+// 4. Ausgabe im Ergebnisbereich der Webseite und der Konsole
---
+++ kap02/lib/js/solution2.js
@@ -0,0 +1,16 @@
+"use strict";
+
+// Ein Array liegt vor
+let numbers = [1, 2, 3, 4, 5];
+
+/*
+Die folgenden Schritte 1 - 3 sollen durch Methodenverkettung als eine Anweisung durchgeführt werden. Dabei kann jeweils die Lambda-Notation verwendet werden.
+*/
+let result = numbers.map(num => num * 2)  // 1. Jedes Element im Array soll vom Wert verdoppelt werden 
+.filter(num => num > 5) // 2. Danach filtern aller Zahlen > 5 und Rückgabe des Ergebnisses
+.reduce((sum, num) => sum + num, 0); // 3. Reduzieren des Arrays auf einzelnen Wert mit der Summe der Elemente
+
+// 4. Ausgabe im Ergebnisbereich der Webseite und in der Konsole
+
+document.getElementById("erg").innerHTML=result;  // Gibt 24 aus (6 + 8 + 10)
+console.log(result);
